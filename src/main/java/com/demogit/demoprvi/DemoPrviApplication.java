91f5de04cd1937ec5a263b75d0b040b22880fc7d
Ivan Toskov <ivan.toskov@endava.com>
2020-03-05 14:07:36 +0100

    initial commmit

---
+++ src/main/java/com/demogit/demoprvi/DemoPrviApplication.java
@@ -0,0 +1,13 @@
+package com.demogit.demoprvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPrviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPrviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demogit/demoprvi/DemoPrviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demogit.demoprvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPrviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
